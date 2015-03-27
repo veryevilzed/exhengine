9ebb3cfa90a51f0b062e37aa9b0460bcea8f338f
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2015-03-27 10:56:10 +0300

    Initial

---
+++ exh/engine/ExSprite.hx
@@ -0,0 +1,12 @@
+package exh.engine;
+
+import openfl.display.Sprite;
+import flash.geom.Rectangle;
+
+class ExSprite extends Sprite {
+
+    var BoundBox:Rectangle = new Rectangle(0,0,0,0);
+
+    
+
+}
